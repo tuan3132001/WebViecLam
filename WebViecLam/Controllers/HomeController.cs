dca168c09c42e09212609dfe81e28a0b033e4e90
Anh Tuan <leanhtuan3103@gmail.com>
2023-07-12 20:45:53 +0700

    first commit

---
+++ WebViecLam/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebViecLam.Models;
+
+namespace WebViecLam.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
